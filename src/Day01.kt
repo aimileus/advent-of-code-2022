940cf518a11ed118cede903ed7fde393cf178aa6
Emiel Wiedijk <me@aimileus.nl>
2022-12-01 14:13:28 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,19 @@
 fun main() {
+    fun elves(input: List<String>) =
+        input.map { it.toIntOrNull() }.splitOnNull().map { it.sum() }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return elves(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return elves(input).sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -7,6 +7,27 @@ import java.security.MessageDigest
  */
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
+fun <T> Iterable<T>.splitOn(el: T): Sequence<List<T>> {
+    return sequence {
+        var cur = mutableListOf<T>()
+        for (x in this@splitOn) {
+            if (x == el) {
+                yield(cur)
+                cur = mutableListOf()
+            } else {
+                cur.add(x)
+            }
+        }
+
+        yield(cur)
+    }
+}
+
+fun <T> Iterable<T?>.splitOnNull(): Sequence<List<T>> {
+    @Suppress("UNCHECKED_CAST")
+    return this.splitOn(null) as Sequence<List<T>>
+}
+
 /**
  * Converts string to md5 hash.
  */
